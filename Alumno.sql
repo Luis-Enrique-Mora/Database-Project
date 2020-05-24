e91afe2dba9104f7ec72e922e9f1099166626975
Luis Enrique Mora <luisenriquemora71829310112@protonmail.com>
2020-05-23 19:43:16 -0600

    Se creo la llave primaria de la tabla alumno llamada PK_Alumno

--- Alumno.sql
+++ Alumno.sql
@@ -7,12 +7,12 @@ Go
 Create Table Alumno
 (
 	Alumno_Id int Identity (1,1),
-	Cedula Varchar(25) not null,
+	Cedula Varchar(25) unique not null,
 	Nombre Varchar(50) not null,
 	Apellido1 Varchar(25) not null,
 	Apellido2 Varchar(25) not null,
 	Edad int not null,
 	Direccion Varchar(200),
-
+	Constraint PK_Alumno Primary Key (Alumno_Id)
 )
 Go
