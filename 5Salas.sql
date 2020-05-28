e858d321da8e11513027311cb6cbc892010f7334
Luis Enrique Mora <luisenriquemora71829310112@protonmail.com>
2020-05-28 15:55:11 -0600

    organizando los archivos y creando la tabla salas

---
+++ 5Salas.sql
@@ -0,0 +1,12 @@
+use SuperStarGymServer
+Go
+Drop table if Exists salas
+Go
+Create Table salas
+(
+	sala_id int Identity(1,1) not null,
+	nombre_sala varchar(20),
+	--Constraints
+	--PK
+	Constraint PK_sala Primary Key (sala_id)
+)
--- 6Clase.sql
+++ 6Clase.sql
@@ -8,6 +8,7 @@ Create table clases
 (
 	limite_inscripcion int not null,
 	total_alumnos int not null,
+	sala_fk int not null,
 	fecha_hora DateTime not null,
 	actividad_cod_fk int not null,
 	--constraints
@@ -15,6 +16,7 @@ Create table clases
 	--The clustered key was created to search data faster, data will be found faster when is searched by actividad_cod_fk or fecha_hora
 	Constraint PK_Clase Primary Key clustered (actividad_cod_fk, fecha_hora),
 	--FK
-	Constraint FK_actividad_clase Foreign key (actividad_cod_fk) References actividades(actividad_cod)
+	Constraint FK_actividad_clase Foreign key (actividad_cod_fk) References actividades(actividad_cod),
+	Constraint FK_sala_clase Foreign Key (sala_fk) References salas(sala_id)
 )
 Go
