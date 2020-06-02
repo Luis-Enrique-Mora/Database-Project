ed02101d23ea97d7e1c230b2b6fe76566f854810
keivintoruno <keivin.toruno.jimenezz@est.una.ac.cr>
2020-06-02 15:01:39 -0600

    actualizar

--- StoredProcedures.sql
+++ StoredProcedures.sql
@@ -400,13 +400,13 @@ GO
 Create PROC SP_BuscarUsuarios(@usuario_Id int)
 AS
 IF (@usuario_Id = '')
-BEGIN
-PRINT 'NO SE PUEDEN INGRESAR VALORES NULOS'
-END
+   BEGIN
+      PRINT 'NO SE PUEDEN INGRESAR VALORES NULOS'
+   END
 ELSE
-BEGIN
-select contrasena_usuario, persona_fk FROM usuarios where usuario_Id=@usuario_Id
-END
+   BEGIN
+      select contrasena_usuario, persona_fk FROM usuarios where usuario_Id=@usuario_Id
+   END
 GO
  
 EXEC SP_BuscarUsuarios '1'
@@ -522,3 +522,61 @@ GO
 
 EXEC SP_BuscarSalas '1'
 GO
+
+
+----------------------- BACKUP ----------------
+Use SuperStarGymServer
+Go
+create proc full_backup
+As
+	Begin
+		Backup database SuperStarGymServer
+		to disk = 'C:\DBBackUp\Backup_super_star_gym.bak'
+		With name ='Gym Full backup',
+		Description = 'full back up de la base de datos SuperStarGymServer'
+	End
+Go
+Use SuperStarGymServer
+Go
+create proc differential_backup
+As
+	Begin
+		Backup database SuperStarGymServer
+		to disk = 'C:\DBBackUp\Backup_super_star_gym.bak'
+		With name ='Gym differential backup',
+		Description = 'differential back up de la base de datos SuperStarGymServer',
+		Differential
+	End
+Go
+
+Use SuperStarGymServer
+Go
+create proc log_backup
+As
+	Begin
+		Backup Log SuperStarGymServer
+		to disk = 'C:\DBBackUp\Backup_super_star_gym.bak'
+		With 
+		name ='Gym log backup',
+		Description = 'log back up de la base de datos SuperStarGymServer'
+	End
+Go
+
+Use master
+Go
+create proc restore_database
+As
+	Begin
+		Restore database SuperStarGymServer
+		From disk = 'C:\DBBackUp\Backup_super_star_gym.bak'
+		With file = 1, NoRecovery
+
+		Restore database SuperStarGymServer
+		From disk = 'C:\DBBackUp\Backup_super_star_gym.bak'
+		With file = 2, NoRecovery
+
+		Restore database SuperStarGymServer
+		From disk = 'C:\DBBackUp\Backup_super_star_gym.bak'
+		With file = 3, Recovery
+	End
+Go
